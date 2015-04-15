6a74cb17e3e71bcfd91435bb1705bdad4b0e6923
tristaoeast <tristaoeast@gmail.com>
2015-04-15 14:01:31 +0100

    stuff

--- PuppetMaster/PuppetMasterGUI.Designer.cs
+++ PuppetMaster/PuppetMasterGUI.Designer.cs
@@ -28,17 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tb_submit = new System.Windows.Forms.TextBox();
+            this.tb_Submit = new System.Windows.Forms.TextBox();
             this.lbl_submit = new System.Windows.Forms.Label();
-            this.bt_submit = new System.Windows.Forms.Button();
+            this.bt_Submit = new System.Windows.Forms.Button();
+            this.tb_Output = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // tb_submit
+            // tb_Submit
             // 
-            this.tb_submit.Location = new System.Drawing.Point(109, 9);
-            this.tb_submit.Name = "tb_submit";
-            this.tb_submit.Size = new System.Drawing.Size(691, 20);
-            this.tb_submit.TabIndex = 0;
+            this.tb_Submit.Location = new System.Drawing.Point(109, 9);
+            this.tb_Submit.Name = "tb_Submit";
+            this.tb_Submit.Size = new System.Drawing.Size(691, 20);
+            this.tb_Submit.TabIndex = 0;
             // 
             // lbl_submit
             // 
@@ -50,24 +51,33 @@
             this.lbl_submit.Text = "Command Submit";
             this.lbl_submit.UseMnemonic = false;
             // 
-            // bt_submit
+            // bt_Submit
             // 
-            this.bt_submit.Location = new System.Drawing.Point(806, 6);
-            this.bt_submit.Name = "bt_submit";
-            this.bt_submit.Size = new System.Drawing.Size(75, 23);
-            this.bt_submit.TabIndex = 2;
-            this.bt_submit.Text = "Submit";
-            this.bt_submit.UseVisualStyleBackColor = true;
-            this.bt_submit.Click += new System.EventHandler(this.Submit);
+            this.bt_Submit.Location = new System.Drawing.Point(806, 6);
+            this.bt_Submit.Name = "bt_Submit";
+            this.bt_Submit.Size = new System.Drawing.Size(75, 23);
+            this.bt_Submit.TabIndex = 2;
+            this.bt_Submit.Text = "Submit";
+            this.bt_Submit.UseVisualStyleBackColor = true;
+            this.bt_Submit.Click += new System.EventHandler(this.bt_submit_Click);
+            // 
+            // tb_Output
+            // 
+            this.tb_Output.Location = new System.Drawing.Point(109, 73);
+            this.tb_Output.Multiline = true;
+            this.tb_Output.Name = "tb_Output";
+            this.tb_Output.Size = new System.Drawing.Size(691, 195);
+            this.tb_Output.TabIndex = 3;
             // 
             // PuppetMasterGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(911, 304);
-            this.Controls.Add(this.bt_submit);
+            this.Controls.Add(this.tb_Output);
+            this.Controls.Add(this.bt_Submit);
             this.Controls.Add(this.lbl_submit);
-            this.Controls.Add(this.tb_submit);
+            this.Controls.Add(this.tb_Submit);
             this.Name = "PuppetMasterGUI";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -77,9 +87,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox tb_submit;
+        private System.Windows.Forms.TextBox tb_Submit;
         private System.Windows.Forms.Label lbl_submit;
-        private System.Windows.Forms.Button bt_submit;
+        private System.Windows.Forms.Button bt_Submit;
+        private System.Windows.Forms.TextBox tb_Output;
     }
 }
 
--- PuppetMaster/PuppetMasterGUI.cs
+++ PuppetMaster/PuppetMasterGUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,15 +13,76 @@ namespace PuppetMaster
 {
     public partial class PuppetMasterGUI : Form
     {
+        private String command;
+        private String args;
+
         public PuppetMasterGUI()
         {
             InitializeComponent();
         }
 
-        private void Submit(object sender, EventArgs e)
+        private void Init(String EntryURL)
         {
 
         }
- 
+
+        private void bt_submit_Click(object sender, EventArgs e)
+        {
+            Process.Start("C:\\");
+
+            String submittedText;
+            if (!string.IsNullOrWhiteSpace(tb_Submit.Text))
+            {
+                submittedText = tb_Submit.Text;
+
+                String[] split = submittedText.Split('(');
+                command = split[0];
+                int pFrom = submittedText.IndexOf("(") + "(".Length;
+                int pTo = submittedText.LastIndexOf(")");
+                if (pTo - pFrom > pFrom)
+                    args = submittedText.Substring(pFrom, pTo - pFrom);
+                else
+                {
+                    tb_Output.AppendText("Wrong command format. Must be something like command(args..)\r\n");
+                    return;
+                }
+
+                if (command.Equals("Submit", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("Worker", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("Wait", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("Status", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("SlowW", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("FreezeW", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("UnfreezeW", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("FreezeC", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else if (command.Equals("UnfreezeC", StringComparison.InvariantCultureIgnoreCase))
+                {
+                }
+                else
+                {
+                    tb_Output.AppendText("Invalid command\r\n");
+                    return;
+                }
+            }
+            else
+                tb_Output.AppendText("Please enter a command\r\n");
+        }
+
     }
 }
